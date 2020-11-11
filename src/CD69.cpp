206515c16624fed72bf75de5c4783dfcc6073565
iDestro <573267381@qq.com>
2020-11-11 23:59:11 +0800

    hhh

---
+++ src/CD69.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int unformed_sum(vector<int>& arr) {
+    sort(arr.begin(), arr.end());
+    int range = 0;
+    for (int i = 0; i < arr.size(); i++)
+
+}
+
+int main() {
+
+    return 0;
+}
